dae638be36ab7afc328136fbfc4fe42e19e26921
Ekaterina <kattren990703@gmail.com>
2021-05-17 16:40:53 +0500

    adding new pages

--- ggmania/routes/web.php
+++ ggmania/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/join', function () {
     return view('welcome');
+});
+//About our project
+
+Route::get('/about', function () {
+    return view('about');
 });
