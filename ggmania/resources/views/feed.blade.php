6184c1353b3cb696d50cb27705bae8e4f0688cbe
Bekzod <b.askarov@student.inha.uz >
2021-05-18 22:20:29 +0500

    Fetch info from ArticleModel to feed.blade

--- ggmania/resources/views/feed.blade.php
+++ ggmania/resources/views/feed.blade.php
@@ -1,1 +1,15 @@
-<
+@foreach($all_articles as $article)
+    <table>
+<thead>
+<td>Title</td>
+<td>Content</td>
+</thead>
+
+<tbody>
+
+<td>{{$article->Title}}</td>
+<td>{{$article->Content}}</td>
+
+</tbody>
+    </table>
+@endforeach
