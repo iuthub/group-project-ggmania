f3af4721cd0648a072cc7c2d86fae3e8b13fbb6b
Bekzod <b.askarov@student.inha.uz >
2021-05-19 09:01:24 +0500

    Added more info from model to view

--- ggmania/resources/views/feed.blade.php
+++ ggmania/resources/views/feed.blade.php
@@ -3,12 +3,17 @@
 <thead>
 <td>Title</td>
 <td>Content</td>
+<td>Created at</td>
+<td>Topic</td>
+
 </thead>
 
 <tbody>
 
 <td>{{$article->Title}}</td>
 <td>{{$article->Content}}</td>
+<td>{{$article->created_at}}</td>
+<td>{{$article->Topic}}</td>
 
 </tbody>
     </table>
