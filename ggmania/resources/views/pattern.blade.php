bd472380ac5201a73e0304001ea057ad5483a368
Ekaterina <kattren990703@gmail.com>
2021-05-20 23:24:28 +0500

    adding pattern for files

---
+++ ggmania/resources/views/pattern.blade.php
@@ -0,0 +1,26 @@
+<!-- Creating a pattern for pages -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ggmania</title>
+    <link rel="stylesheet" href="{{ asset('/css/pattern.css') }}"/>
+</head>
+<body>
+<header>
+    <h2>Logo</h2>
+    <div class="navigation">
+        <a href="">Home</a>
+        <a href="">Categories</a>
+        <a href="">Add article</a>
+        <a href="">Log in</a>
+    </div>
+</header>
+ @yield('content')
+<footer>
+    <h2>&copy; Team-GGmania</h2>
+    <p> interesting</p>
+</footer>
+</body>
+</html>
