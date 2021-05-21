0cc7a2451f982a99067d55db488af54b11b862c0
Ekaterina <kattren990703@gmail.com>
2021-05-21 23:26:34 +0500

    updating login  page

--- ggmania/resources/views/welcome.blade.php
+++ ggmania/resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
     </head>
     <body>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500&display=swap" rel="stylesheet">    <div class="textbox">
-
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500&display=swap" rel="stylesheet">
+    <div class="textbox">
         <img src="{{ asset('/gglong.jpg') }}" alt="Logo" height="100" width="400">
         <h2>
             GGmania&mdash;community of gamers and source of sharp opinions!
@@ -18,23 +18,14 @@
     <div class="container">
         <form method="post" action="{{route('login')}}" class="form">
             @csrf
-
-            <p class="login-text" style="font-size:2em;font-weight: 800;">Log in</p>
-            <label>
-                <input name="email" type="text" class="text" placeholder="Enter your Email or Username" required>
-            </label>
-            <label>
-                <input name="password" type="password" class="text" placeholder="Password" required>
-            </label>
-            <button class="login">Log in</button>
-
+            <div class="login-text"> <p style="font-size:40px;font-weight: 800;font-family: Roboto;">Log in</p></div>
+            <div class="input">
+                <label><input name="email" type="text" class="text" placeholder="Enter your Email or Username" required></label>
+                <label><input name="password" type="password" class="text" placeholder="Password" required></label>
+            </div>
+            <div class="login"><button class="login">Log in</button></div>
+            <div class="login-regiter-text"><button class="register"><a href="/register">Create account</a></button></div>
         </form>
-        <div class="login-regiter-text">
-
-            <button class="register"><a href="/register">Create account</a></button>
-
-        </div>
-
     </div>
     <footer>
         <h2>&copy; Team-GGmania</h2>
