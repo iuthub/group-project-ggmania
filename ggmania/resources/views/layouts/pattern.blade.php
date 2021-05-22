20710493865d971c4b39aa0c898e8cef35bd4e3c
Ekaterina <kattren990703@gmail.com>
2021-05-22 11:04:24 +0500

    header update

--- ggmania/resources/views/layouts/pattern.blade.php
+++ ggmania/resources/views/layouts/pattern.blade.php
@@ -12,15 +12,19 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500&display=swap" rel="stylesheet">
 @yield('text')
-<header>
-    <img src="{{ asset('/ggmaniamain.jpg') }}" alt="Logo">
+<div class="container">
+    <header class="header">
+
+        <div class="logo"><img src="{{ asset('/ggmaniamain.jpg') }}" alt="Logo"></div>
     <div class="navigation">
-        <a href="">Home</a>
-        <a href="">Categories</a>
-        <a href="">Add article</a>
-        <a href="">Log in</a>
+        <ul>
+            <li><a href="/prfile">Log in</a></li>
+            <li><a href="">Add article</a></li>
+            <li><a href="">Categories</a></li>
+            <li><a href="">Home</a></li>
+        </ul>
     </div>
-</header>
+    </header>
 <main>
     @yield('content')
 </main>
@@ -59,6 +63,7 @@
 
     </form>
 </footer>
+</div>
 </body>
 </html>
 
