0003f1bbf649f9bb16ba977d61dc9bccf36d2d3a
Ekaterina <kattren990703@gmail.com>
2021-05-22 14:56:53 +0500

    updating

--- ggmania/resources/views/profile.blade.php
+++ ggmania/resources/views/profile.blade.php
@@ -4,7 +4,7 @@
 {{--    {"id":1,"name":"Shamsiddin","email":"quyosh.ogli@gmail.com","email_verified_at":null,"created_at":"2021-05-20T20:16:31.000000Z","updated_at":"2021-05-20T20:16:31.000000Z","passport_number":"","birth_date":null,"username":"","phone_number":0,"postal_code":0,"city":"","last_name":""}--}}
 <div class="container profile-page">
     <div class="profile-page-header">
-        <img src="" alt="" class="user-avatar">
+        <img src="{{ asset('/avatar.png') }}" alt="" class="user-avatar">
         <div class="main-user-info">
             <span>Name: {{Auth::user()->name}}</span>
             <span>Last name: {{Auth::user()->last_name}}</span>
@@ -21,14 +21,19 @@
 @endsection
 
 <style>
+    .container
+    {
+        background-color: #ffffff;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+        padding: 25px 30px;
+        border-radius: 12px;
+    }
     .profile-page{
         padding: 5vh;
-        width: 100%;
-        height: 100%;
-
+        width: 600px;
+        height: 600px;
         display: flex;
         flex-direction: column;
-
         align-items: center;
         justify-content: center;
     }
@@ -37,14 +42,12 @@
         width: 100%;
         display: flex;
         flex-direction: row;
-
         align-items: center;
         justify-content: center;
     }
     .user-avatar{
-        width: 20%;
-        height: 100%;
-        background: #999;
+        width: 200px;
+        height: 200px;
     }
     .main-user-info{
         width: 70%;
@@ -67,6 +70,6 @@
     }
     .profile-page-body span{
         width: 100%;
-        text-align: left;
+        text-align: center;
     }
 </style>
