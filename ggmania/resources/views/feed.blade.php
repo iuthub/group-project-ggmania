141ca1b37d1fdfcc8493278a2066f4cdd808512b
Ekaterina <kattren990703@gmail.com>
2021-05-22 15:47:41 +0500

    update style for feed

--- ggmania/resources/views/feed.blade.php
+++ ggmania/resources/views/feed.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.pattern')
 @section('content')
-
+   <div class="arttop"><h1>Articles</h1></div>
+    <link rel="stylesheet" href="{{ asset('/css/feed.css') }}"/>
 @foreach($all_articles as $article)
-    <table>
+    <div class="container">
+        <table>
 <thead>
 {{--*/<a href="/feed/{{$article->id}}">--}}
 <td>Title</td>
@@ -27,7 +29,7 @@
 
 @endif
 </tbody>
-    </table>
+        </table></div>
 @endforeach
 <div class="container">
     @if(Session::has('deleted'))
