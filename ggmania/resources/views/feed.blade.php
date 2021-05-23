028e876db27b411fa3c5ba71fbb69c291a56740a
Bekzod <b.askarov@student.inha.uz >
2021-05-23 13:22:47 +0500

    feed article preview limit length

--- ggmania/resources/views/feed.blade.php
+++ ggmania/resources/views/feed.blade.php
@@ -2,28 +2,32 @@
 @section('content')
    <div class="arttop"><h1>Articles</h1></div>
     <link rel="stylesheet" href="{{ asset('/css/feed.css') }}"/>
-@foreach($all_articles as $article)<div class="middle">
-<div class="col-md-4">
-    <img src="cardimage.png" class="img-fluid">
+@foreach($all_articles as $article)
 
-</div>
+    <div class="middle">
+
     <div class="col-lg-8 col-md-12 col-sm-12">
 <h1>{{$article->Title}}</h1>
 <h5>Content</h5>
 <h5>Created at</h5>
 <h5>Topic</h5>
 {{$article->Title}}
-<td>{{$article->Content}}</td>
+        @if (strlen ($article->Content)>100){{substr($article->Content, 0, 100)}}...
+        @else
+            {{$article->Content}}
+    @endif
+
 <td>{{$article->created_at->format('Y-m-d')}}</td>
 <td>{{$article->Topic}}</td>
         <h5>{{$article->username}}</h5>
-{{--@if (Auth::user()->username = $article->username)              --}}
+             --}}
 @if (auth()->user()->username === $article->username)
 <button><a href="{{"delete/".$article['id']}}">Delete</a><a/></button>
+        @endif
+</div>
+    </div>
 
-</div>
-@endif
-</div>
+
 @endforeach
 <div>
     @if(Session::has('deleted'))
