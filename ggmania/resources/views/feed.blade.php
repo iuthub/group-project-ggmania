22bffb81297a00658a5b05b9e148eae8a284ad60
Ekaterina <kattren990703@gmail.com>
2021-05-23 13:20:42 +0500

    pdate feed cards

--- ggmania/resources/views/feed.blade.php
+++ ggmania/resources/views/feed.blade.php
@@ -3,20 +3,17 @@
    <div class="arttop"><h1>Articles</h1></div>
     <link rel="stylesheet" href="{{ asset('/css/feed.css') }}"/>
 @foreach($all_articles as $article)<div class="middle">
-<div class="col-md-4">
-    <img src="cardimage.png" class="img-fluid">
+<div class="col-md-3">
+    <img src="articleimg.jpg" class="img-fluid" alt="article img">
 
 </div>
     <div class="col-lg-8 col-md-12 col-sm-12">
 <h1>{{$article->Title}}</h1>
-<h5>Content</h5>
-<h5>Created at</h5>
-<h5>Topic</h5>
-{{$article->Title}}
-<td>{{$article->Content}}</td>
-<td>{{$article->created_at->format('Y-m-d')}}</td>
-<td>{{$article->Topic}}</td>
-        <h5>{{$article->username}}</h5>
+<h5>Content:</h5>
+        <p>{{$article->Content}}</p>
+<h5>Created at: {{$article->created_at->format('Y-m-d')}}</h5>
+<h5>Topic: {{$article->Topic}}</h5>
+        <h5>Username: {{$article->username}}</h5>
 {{--@if (Auth::user()->username = $article->username)              --}}
 @if (auth()->user()->username === $article->username)
 <button><a href="{{"delete/".$article['id']}}">Delete</a><a/></button>
